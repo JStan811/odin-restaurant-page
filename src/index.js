7eb7743512f32fb707355c403244ed14a938719f
Jake Standley <jakestandley95@gmail.com>
2022-01-27 11:07:02 -0800

    Add switch tab event listeners
    Here I grabbed reference to each nav menu item, then added a switchTab function as a click event listener function to each. switchTab takes a tabRender function (on of the tab render I have as the default export of each tab module). switchTab first changes the background of each menu item to mistyrose (the body background) before running the render. This is because each render sets its respective menu item background to white so that color persists on each tab, making it obvious which tab the user is one. By setting each item to mistyRose with switchTab, that white color change is not permanent after a tab is switched to the first time.
    
    At this point, the project is "done". I say "done" because while I've met all of the project requirements, the final product is pretty lackluster. It just meets the requirements but does not really go above or beyond anywhere. The content and design leaves much to be desired. If I were to use this for a portfolio project (I doubt I would), I would then make it much better, but at this point I'm going to call it a day and move on with the class.

--- src/index.js
+++ src/index.js
@@ -5,4 +5,28 @@ import renderContactTab from './contactTab'
 // write main code as IIFE to not polute global scope
 (function () {
   renderHomeTab();
+
+  const homeTabMenuItem = document.querySelector('#home');
+  const menuTabMenuItem = document.querySelector('#menu');
+  const contactTabMenuItem = document.querySelector('#contact');
+
+  const switchTabs = function(tabRender) {
+    const contentDiv = document.querySelector('#content');
+    contentDiv.remove();
+
+    homeTabMenuItem.style.background = 'mistyrose';
+    menuTabMenuItem.style.background = 'mistyrose';
+    contactTabMenuItem.style.background = 'mistyrose';
+    tabRender();
+  }
+
+  homeTabMenuItem.addEventListener('click', () => {
+    switchTabs(renderHomeTab)
+  });
+  menuTabMenuItem.addEventListener('click', () => {
+    switchTabs(renderMenuTab)
+  });
+  contactTabMenuItem.addEventListener('click', () => {
+    switchTabs(renderContactTab)
+  });
 })();
